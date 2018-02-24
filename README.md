ea38c3c4ee3a818c22e2ed0abf5f301ac2ea18c8
Ahowe042 <ahowe042@gmail.com>
2018-02-23 22:59:50 -0500

    JavaScript Update
    Added new JavaScript code, separated in .js file. Added HTML to provide an animated example of new JavaScript code. Updated ReadMe with short blurb.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testJS
 Beginning of Javascript Projects
+
+This repository will serve as both a marker of my progress and a means to teach others some of the key aspects of JavaScript
--- test1.js
+++ test1.js
@@ -1,9 +1,16 @@
-function changeFormat() {
-  document.getElementById("formatP2").innerHTML = "This is the newly formatted paragraph, complete with color change, font change, and <b>bolded letters.</b>"
-  document.getElementById("formatD2").innerHTML = "<ol> <li>Whoa</li> <li>This</li> <li>Totally</li> <li>Worked!</li> </ol>"
-}
 
+// This function (called by the OLD Format button) reverts the paragraph text and styling to its original form.
+// The list numbering style is also reverted to "I" for Roman Numerals
 function revertFormat() {
   document.getElementById("formatP2").innerHTML = "This is the original paragraph. As you can see, no special formatting has been applied to this paragraph."
-  document.getElementById("formatD2").innerHTML = "<ul id=\"formatL2\" style=\"list-style-type:disc\"> <li>Lions</li> <li>Tigers</li> <li>Bears</li> <li>Oh my!</li> </ul>"
+  document.getElementById("formatP2").style = "color:black; font-family:serif"
+  document.getElementById("formatL2").type = "I"
+}
+
+// This function (called by the NEW Format button) completely changes the paragraph text along with its font styling.
+// It also changes the ordered list numbering style to "A", which is alphabetical.
+function changeFormat() {
+  document.getElementById("formatP2").innerHTML = "This is the newly formatted paragraph, complete with color change, font change, and <b>bolded letters.</b> Also, notice how the list below changed as well."
+  document.getElementById("formatP2").style = "color:blue; font-family:sans-serif"
+  document.getElementById("formatL2").type = "A"
 }
